dbd6024c7d26f2ed1c38749ed3f3ac0fb25d06ae
villasana26enrique <enrique.villasana26@gmail.com>
2020-04-30 16:22:43 -0400

    Trabajando en mi primer proyecto AngularJs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'hola-mundo';
+  name = 'Enrique Villasana';
+  lenguage = 'Angular';
+  calificacion = 'Bueno';
 }
