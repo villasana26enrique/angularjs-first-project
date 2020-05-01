b17bd22a7a41b291c14a92e5757da70f4218a10d
villasana26enrique <enrique.villasana26@gmail.com>
2020-04-30 22:15:47 -0400

    Funcionalidad del Body (NgIf y NgFor)

--- src/app/components/body/body.component.ts
+++ src/app/components/body/body.component.ts
@@ -4,4 +4,18 @@ import { Component } from '@angular/core';
     selector: 'app-body',
     templateUrl: './body.component.html'
 })
-export class BodyComponent{}
+export class BodyComponent{
+    frase: any;
+    mostrar: boolean;
+    familiares: string[];
+    texto: string;
+    constructor() {
+        this.frase = {
+            mensaje: 'Ahora me ves!',
+            autor: 'Enrique Villasana'
+        };
+        this.mostrar = true;
+        this.familiares = ['Trina', 'Yezeina', 'Enrique', 'Maria Elisa', 'Jonathan', 'Junior'];
+        this.texto = 'Mostrar/Ocultar';
+    }
+}
